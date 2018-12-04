90b2ecc0dc61361ebd4906159c532f64aede55e6
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2018-12-04 16:20:20 +0100

    Added LeaderboardController

---
+++ Ullechamp-Api.RestApi/Controllers/LeaderboardController.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Ullechamp_Api.Core.ApplicationService;
+using Ullechamp_Api.Core.Entity;
+
+namespace Ullechamp_Api.RestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LeaderboardController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public LeaderboardController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return Ok(_userService.GetAllUsers());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = _userService.GetById(id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+            return Ok(_userService.CreateUser(user));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult<User> Put(int id, [FromBody] User user)
+        {
+            if (!id.Equals(user.Id))
+            {
+                return BadRequest("Parameter ID and User ID must be the same! Bitch");
+            }
+
+            return _userService.Update(user);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<User> Delete(int id)
+        {
+            return _userService.Delete(id);
+        }
+    }
+}
